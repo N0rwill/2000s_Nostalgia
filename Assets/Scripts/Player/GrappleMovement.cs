a26c0482073e504d99d7e13478ba695a6a45c889
EdenPhiila <luxening@gmail.com>
2025-04-10 17:18:21 -0400

    Lock on test

--- Assets/Scripts/Player/GrappleMovement.cs
+++ Assets/Scripts/Player/GrappleMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GrappleMovement : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class GrappleMovement : MonoBehaviour
     public GameObject grappleObject;
 
     private Vector3 grapplePoint;
+    public Vector3 launchToPoint;
 
     [Header("Pulling")]
     public float pullForce = 25f;
@@ -31,19 +33,26 @@ public class GrappleMovement : MonoBehaviour
 
     private bool grappling;
 
+    public bool canSeeGrapple = false;
+
+    
+
     void Start()
     {
+
         pm = GetComponent<PlayerMovement>();
+
     }
 
     void Update()
     {
+
         if (Input.GetKeyDown(grappleKey))
         {
             StartGrapple();
         }
 
-        if (grapplingCdTimer > 0)
+        if (grapplingCdTimer > 0) //constant cooldown timer. When 0, can grapple again
         {
             grapplingCdTimer -= Time.deltaTime;
         }
@@ -53,78 +62,89 @@ public class GrappleMovement : MonoBehaviour
     {
         if (grappling) 
         {
-            lr.SetPosition(0, stickyhand.position);
+            lr.SetPosition(0, stickyhand.position); //set the line renderer from you to wherever the stickyhand connects while grappling.
         }
     }
 
     private void StartGrapple() 
     {
-        if (grapplingCdTimer > 0) return;
+        if (grapplingCdTimer > 0) return; //fails grapple if cooldown
 
         grappling = true;
 
         RaycastHit hit;
 
-        if (Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance, grapple))
+        if (Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance, grapple)) //sends out a raycast to see if it hits a launch object, a pull object, or nothing.
         {
-            if (hit.collider.gameObject.CompareTag("GrappleLaunch"))
+            if (hit.collider.gameObject.CompareTag("GrapplePull"))
             {
-                pm.freeze = true;
+                grapplePoint = hit.point; //Again, point where the grapple hits
 
-                grapplePoint = hit.point;
-
-                grappleObject = hit.collider.gameObject;
-
-                Invoke(nameof(ExecuteGrappleLaunch), grappleDelayTime);
-            }
-            else if (hit.collider.gameObject.CompareTag("GrapplePull"))
-            {
-                grapplePoint = hit.point;
-
-                grappleObject = hit.collider.gameObject;
+                grappleObject = hit.collider.gameObject; //Again, game object that the player just hit with the grapple.
 
                 Invoke(nameof(ExecuteGrapplePull), grappleDelayTime);
             }
-            else
-            {
-                grapplePoint = cam.position + cam.forward * maxGrappleDistance;
-
-                Invoke(nameof(StopGrapple), grappleDelayTime);
-            }
-
         }
+
+        else if (canSeeGrapple)
+        {
+            if (Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance, grapple))
+            {
+
+                pm.freeze = true; //freeze the player for dramatic effect
+
+                grapplePoint = hit.point; //point where the grapple hits
+
+                launchToPoint = hit.collider.gameObject.GetComponent<LaunchPoint>().launchPoint; //point where the player will fly to.
+
+                grappleObject = hit.collider.gameObject; //GameObject that the player hit.
+
+                Invoke(nameof(ExecuteGrappleLaunch), grappleDelayTime);
+            }
+        }
+
+        else
+        {
+            grapplePoint = cam.position + cam.forward * maxGrappleDistance; //If the player misses anything, just put the grapple out the max distance
+
+            Invoke(nameof(StopGrapple), grappleDelayTime); //And then stop the grapple.
+        }
+
+        
         lr.enabled = true;
-        lr.SetPosition(1, grapplePoint);
+        lr.SetPosition(1, grapplePoint); //Create a line renderer for the grapple, launching it out to the point the player connects with, or max distance.
     }
 
     private void ExecuteGrapplePull() 
     {
-        pm.freeze = false;
+        pm.freeze = false; //unfreeze the player
 
-        grappleObject.GetComponent<GrapplePulling>().pullForce = pullForce;
+        grappleObject.GetComponent<GrapplePulling>().pullForce = pullForce; //Pull the game object at the force that the player has on it.
         grappleObject.GetComponent<GrapplePulling>().pull = true;
 
-        Invoke(nameof(StopGrapple), 1f);
+        Invoke(nameof(StopGrapple), 1f); //Only grapple for a little bit
     }
 
     private void ExecuteGrappleLaunch() 
     {
-        pm.freeze = false;
+        pm.freeze = false; //unfreeze the player
 
-        Vector3 lowestPoint = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
+        Vector3 lowestPoint = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z); //The bottom of the player
 
-        float grapplePointRelativeYPos = grapplePoint.y - lowestPoint.y;
-        float highestPointOnArc = grapplePointRelativeYPos + overshootYAxis;
+        float grapplePointRelativeYPos = launchToPoint.y - lowestPoint.y; //Calculate the difference between player Y Pos and grapple point Y Pos
+        float highestPointOnArc = grapplePointRelativeYPos + overshootYAxis; //Highest point on launch will be the difference between the two Y Pos, plus whatever arc you give it.
 
-        if (grapplePointRelativeYPos < 0) highestPointOnArc = overshootYAxis;
+        if (grapplePointRelativeYPos < 0) highestPointOnArc = overshootYAxis; //If player is higher than the grapple point, just make the highest point and arc the same thing.
 
-        pm.LaunchToPosition(grapplePoint, highestPointOnArc);
+        pm.LaunchToPosition(launchToPoint, highestPointOnArc); //Launch the player to the point along the arc (Calculation in Player Movement)
 
-        Invoke(nameof(StopGrapple), 1f);
+        Invoke(nameof(StopGrapple), 1f); //Only grapple for a little bit
     }
 
     public void StopGrapple() 
     {
+        //Just undo everything, and set the grapple cooldown
+
         pm.freeze = false;
         grappling = false;
         grapplingCdTimer = grapplingCd;
@@ -132,17 +152,14 @@ public class GrappleMovement : MonoBehaviour
         if (grappleObject != null)
         {
             grappleObject.GetComponent<GrapplePulling>().pull = false;
+            grappleObject.GetComponent<GrapplePulling>().pullForce = 0;
             grappleObject = null;
         }
     }
 
     public bool IsGrappling()
     {
+        //Can't grapple while grappling
         return grappling;
-    }
-
-    public Vector3 GetGrapplePoint()
-    {
-        return grapplePoint;
     }
 }
