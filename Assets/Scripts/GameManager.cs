d95c463e0af189359c21e5961c74aac0996d40a9
averiefiedler <avefied@gmail.com>
2025-04-11 19:18:31 -0400

    UI fixes and player prefab updates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private bool paused = false;
+
+    [SerializeField] public GameObject pauseMenu;
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Pause(paused);
+        }
+
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("HubLevel");
         Debug.Log("Start game.");
+    }
+
+    public void Pause(bool paused)
+    {
+        if (paused == false)
+        {
+            paused = true;
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else if (paused == true)
+        {
+            paused = false;
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
     }
 }
