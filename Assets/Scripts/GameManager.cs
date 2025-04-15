3c56663567b8c3e8ff927a69d73cdd6be09de484
averiefiedler <avefied@gmail.com>
2025-04-15 13:57:24 -0400

    fhsjfjsdj

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("HubLevel");
+        SceneManager.LoadScene("WaterLevel");
         Debug.Log("Start game.");
     }
 
