4cf3e6759efba534451f81a7662e63a43cfab609
DarkDrip <pyrotic240@gmail.com>
2025-04-17 16:20:41 -0400

    Update Waypoint Mover Script

--- Assets/Scripts/World Objects/WaypointMover.cs
+++ Assets/Scripts/World Objects/WaypointMover.cs
@@ -14,10 +14,12 @@ public class WaypointMover : MonoBehaviour
     //waypoint target object moving to
     private Transform currentWaypoint;
 
+    public GameObject chase;
 
     // Start is called before the first frame update
     void Start()
     {
+        chase = GameObject.FindWithTag("Chase");
         //set initial position
         currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint, gameObject);
         transform.position = currentWaypoint.position;
@@ -31,16 +33,39 @@ public class WaypointMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    public void Move()
+    {
+        if (currentWaypoint == null)
+        {
+            return;
+        }
+
+        // Move towards the current waypoint
         transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+
+
         if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
         {
             currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint, gameObject);
-            
+
+            // If no next waypoint is found, stop further movement
+            if (currentWaypoint == null)
+            {
+                return;
+            }
         }
 
         // Smoothly rotate towards the current waypoint
-        Vector3 directionToWaypoint = (currentWaypoint.position - transform.position).normalized;
-        Quaternion targetRotation = Quaternion.LookRotation(directionToWaypoint);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * moveSpeed);
+        Vector3 directionToWaypoint = currentWaypoint.position - transform.position;
+        if (directionToWaypoint.sqrMagnitude > 0.001f) // Ensure the direction vector is not zero
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(directionToWaypoint.normalized);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * moveSpeed);
+        }
     }
+
+    
 }
