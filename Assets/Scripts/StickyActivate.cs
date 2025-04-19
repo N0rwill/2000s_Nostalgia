53fb55cb5e04489965bff83c93a9252928653c47
EdenPhiila <luxening@gmail.com>
2025-04-19 19:20:40 -0400

    Sticky UI and Audio pick up

--- Assets/Scripts/StickyActivate.cs
+++ Assets/Scripts/StickyActivate.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class StickyActivate : MonoBehaviour
 {
+    public AudioClip splat;
 
     void OnTriggerEnter(Collider _other) 
     { 
         if (_other.tag == "Player") 
         {
-            Destroy(gameObject);
+            _other.gameObject.GetComponent<AudioSource>().clip = splat;
+            _other.gameObject.GetComponent<AudioSource>().Play();
             _other.gameObject.GetComponent<GrappleMovement>().enabled = true;
             _other.gameObject.transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+            _other.gameObject.transform.GetChild(2).GetChild(0).GetChild(0).GetChild(3).gameObject.SetActive(true);
+            Destroy(gameObject);
         }
     }
 }
