f2db4e8ba1b558f26aaf705898f56c9592291b29
Norwill <willeverett206@gmail.com>
2025-04-20 00:05:59 -0400

    level select done and pause slightly fixed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,10 +9,6 @@ public class GameManager : MonoBehaviour
     // Singleton pattern
     public static GameManager Instance { get; private set; }
     
-    private bool paused = false;
-
-    [SerializeField] public GameObject pauseMenu;
-    
     public bool Level1Complete;
     public bool Level2Complete;
     public bool Level3Complete;
@@ -28,21 +24,6 @@ public class GameManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
-        }
-    }
-
-    public void Update()
-    {
-        if (Input.GetButtonDown("Cancel"))
-        {
-            if (paused == false)
-            {
-                Pause();
-            }
-            else
-            {
-                Resume();
-            }
         }
     }
 
@@ -65,45 +46,5 @@ public class GameManager : MonoBehaviour
         Level3Complete = true;
         SceneManager.LoadScene("HubLevel");
         Debug.Log("Going to Hub Level.");
-    }
-
-    public void StartGame()
-    {
-        SceneManager.LoadScene("WaterLevel");
-        Debug.Log("Start game.");
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-        Debug.Log("Application has quit.");
-    }
-
-    public void ReturnToMenu()
-    {
-       SceneManager.LoadScene("Start");
-        Debug.Log("Returned to Menu.");
-    }
-
-    public void Settings()
-    {
-        Debug.Log("Settings");
-    }
-
-    public void Pause()
-    {
-        pauseMenu.SetActive(true);
-        paused = true;
-        Time.timeScale = 0f;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-    }
-    public void Resume() 
-    {
-            pauseMenu.SetActive(false);
-        paused = false;
-            Time.timeScale = 1f;
-            Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
     }
 }
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool paused = false;
+
+    public GameObject pauseMenu;
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (paused == false)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("WaterLevel");
+        Debug.Log("Start game.");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Application has quit.");
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Start");
+        Debug.Log("Returned to Menu.");
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Settings");
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        paused = true;
+        Time.timeScale = 0f;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        paused = false;
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
