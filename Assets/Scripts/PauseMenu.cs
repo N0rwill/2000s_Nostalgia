e53f47fe01356c90ba3d705e3ea1d0b0a56803d0
averiefiedler <avefied@gmail.com>
2025-04-21 02:07:07 -0400

    final things

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -24,7 +24,7 @@ public class PauseMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("WaterLevel");
+        SceneManager.LoadScene("HubLevel");
         Debug.Log("Start game.");
     }
 
