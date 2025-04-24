5106945a35bd8d539d8a02d21acbcbef7620f312
Norwill <willeverett206@gmail.com>
2025-04-24 17:13:54 -0400

    tree cars fix add checkpoint

--- Assets/Scripts/World Objects/GrappleCheckpoint.cs
+++ Assets/Scripts/World Objects/GrappleCheckpoint.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class GrappleCheckpoint : MonoBehaviour
 {
     public GameObject deathTrigger;
+    public Rigidbody rb;
 
     void OnTriggerEnter(Collider trigger)
     {
         if (trigger.gameObject.CompareTag("Player"))
         {
             deathTrigger.GetComponent<Teleport>().teleportPoint = gameObject.transform;
+            rb.velocity = Vector3.zero;
         }
     }
 }
